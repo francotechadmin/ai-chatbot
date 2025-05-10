48eef630419119389a2935031159dc9ea7ed4069
Gabriel Franco <gabriel.a.franco@invista.com>
2025-05-10 14:13:31 -0500

    feat: Update RefreshIcon SVG for improved design and responsiveness

--- components/icons.tsx
+++ components/icons.tsx
@@ -1249,19 +1249,18 @@ export const FolderIcon = ({ size = 16 }: { size?: number }) => {
 
 export const RefreshIcon = ({ size = 16 }: { size?: number }) => {
   return (
-    <svg
-      height={size}
-      strokeLinejoin="round"
-      viewBox="0 0 24 24"
-      width={size}
-      style={{ color: 'currentcolor' }}
-    >
+  <svg
+    height={size}
+    strokeLinejoin="round"
+    viewBox="0 0 30 30"
+    width={size}
+    style={{ color: 'currentcolor' }}
+  >
+    <path d="M 15 3 C 12.031398 3 9.3028202 4.0834384 7.2070312 5.875 A 1.0001 1.0001 0 1 0 8.5058594 7.3945312 C 10.25407 5.9000929 12.516602 5 15 5 C 20.19656 5 24.450989 8.9379267 24.951172 14 L 22 14 L 26 20 L 30 14 L 26.949219 14 C 26.437925 7.8516588 21.277839 3 15 3 z M 4 10 L 0 16 L 3.0507812 16 C 3.562075 22.148341 8.7221607 27 15 27 C 17.968602 27 20.69718 25.916562 22.792969 24.125 A 1.0001 1.0001 0 1 0 21.494141 22.605469 C 19.74593 24.099907 17.483398 25 15 25 C 9.80344 25 5.5490109 21.062074 5.0488281 16 L 8 16 L 4 10 z"
+    fill="currentcolor"></path>
+  </svg>
 
-      <g id="SVGRepo_bgCarrier" stroke-width="0" /><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" /><g id="SVGRepo_iconCarrier"> <path d="M3 12C3 16.9706 7.02944 21 12 21C14.3051 21 16.4077 20.1334 18 18.7083L21 16M21 12C21 7.02944 16.9706 3 12 3C9.69494 3 7.59227 3.86656 6 5.29168L3 8M21 21V16M21 16H16M3 3V8M3 8H8" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> </g>
-    </svg>
-
-  );
-};
+)}
 
 export const CheckCircleIcon = ({ size = 16 }: { size?: number }) => {
   return (
